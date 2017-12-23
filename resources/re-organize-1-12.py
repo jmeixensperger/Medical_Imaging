11ba0770ae00da9cd32db91e7eb992fca199302f
Spencer <spencertperry23@gmail.com>
2017-12-23 12:07:11 -0800

    fixed script, made subdirectories

--- resources/re-organize-1-12.py
+++ resources/re-organize-1-12.py
@@ -1,30 +1,31 @@
-import os 
+import os
 import shutil
 
 def main() :
-    for i in range(1,13):
-        patientNum = "patient-1_" + str(i)
-        globalPath = "~/Desktop/Medical_Imaging/converted"
-        patientDirPath = globalPath + patientNum
-        patientPic = patientNum + ".jpg"
-        if not os.path.exists(patientDirPath):
-            os.makedirs( patientDirPath )
-        for f in os.listdir(globalPath) :
-            if f.endswith( patientPic ) :
-                patientCurPath = globalPath + "/" + f
-                shutil.move(patientCurPath, patientDirPath )
-        for file in os.listdir( patientDirPath ):
-            Classes = ["healthy", "emphysema", "fibrosis", "ground_glass", "micronodules" ]
-	        for classes in Classes:
-		        classDirPath = patientDirPath + '/' + classes
-		        if not os.path.exists( classDirPath ):
-                    		os.makedirs( classDirPath )
-	    	    	if file.startswith( classes ):
-                    		filePath = globalPath + "/" + file
-                    		classPath = classDirPath + '/' + file
-                    		shutil.move( filePath, classDirPath ) 
-                
-        if os.listdir( patientDirPath ) == []:
-            os.remove(patientDirPath)
+	for i in range(1,13):
+		patientNum = "patient-1_" + str(i)
+		globalPath = "/Users/#ENTER_USER#/Desktop/Medical_Imaging/converted/"
+		patientDirPath = globalPath + patientNum
+		patientPic = patientNum + ".jpg"
+		if not os.path.exists(patientDirPath):
+			os.makedirs( patientDirPath )
+		for f in os.listdir(globalPath) :
+			if f.endswith( patientPic ) :
+				patientCurPath = globalPath  + f
+				shutil.move(patientCurPath, patientDirPath )
+		f = os.listdir(patientDirPath)
+		for file in os.listdir( patientDirPath ):
+			Classes = ["healthy", "emphysema", "fibrosis", "ground_glass", "micronodules" ]
+			for classes in Classes:
+				classDirPath = patientDirPath + '/' + classes
+				if not os.path.exists( classDirPath ):
+					os.makedirs( classDirPath )
+				if file.startswith( classes ):
+					filePath = patientDirPath + '/' + file
+					classPath = classDirPath + '/' + file
+					shutil.move( filePath, classDirPath )
+
+		if os.listdir( patientDirPath ) == []:
+			os.remove(patientDirPath)
 
 main()
