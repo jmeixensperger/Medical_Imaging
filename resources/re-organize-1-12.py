215fa60ec2216a93e73f6b83eb76ffdec2047ed1
Jack Meixensperger <jmeixensperger@gmail.com>
2017-12-23 10:40:59 -0800

    Update re-organize-1-12.py

--- resources/re-organize-1-12.py
+++ resources/re-organize-1-12.py
@@ -12,6 +12,17 @@ def main() :
             if f.endswith( patientPic ) :
                 patientCurPath = "/Users/student/Desktop/converted/" + f
                 shutil.move(patientCurPath, patientDirPath )
+        for file in os.listdir( patientDirPath ):
+            Classes = ["healthy", "emphysema", "fibrosis", "ground_glass", "micronodules" ]
+	        for classes in Classes:
+		        classDirPath = patientDirPath + '/' + classes
+		        if not os.path.exists( classDirPath ):
+                    os.makedirs( classDirPath )
+	    	    if file.startswith( classes ):
+                    filePath = "/Users/student/Desktop/converted/" + file
+                    classPath = classDirPath + '/' + file
+                    shutil.move( filePath, classDirPath ) 
+                
         if os.listdir( patientDirPath ) == []:
             os.remove(patientDirPath)
 
